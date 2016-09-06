fd8e2a9c058459f89ff0d2fcf2d15be27ac4147f
pablochux15@gmail.com <pablochux15@gmail.com>
2016-09-06 18:20:31 +0200

    Separate the .scss files in bem blocks

--- gulpfile.js
+++ gulpfile.js
@@ -9,6 +9,7 @@ var gulp = require('gulp'),
     imagemin = require('gulp-imagemin'),
     autoprefixer = require('gulp-autoprefixer'),
     del = require('del'),
+    jasmine = require('gulp-jasmine'),
     browserSync = require('browser-sync').create(),
     reload = browserSync.reload;
 
@@ -16,48 +17,49 @@ var gulp = require('gulp'),
 var src = 'template/src/',
     minsrc = 'template/dist/';
 
-// TASKS
+// ---------
+// Development
+// ---------
 // Default task starts the server in the src folder (development)
-gulp.task('default', function(){
-	browserSync.init({
+gulp.task('default', function() {
+    browserSync.init({
         server: {
             baseDir: [src, '.tmp'],
-            routes:  {
+            routes: {
                 '/bower_components': 'bower_components'
             }
         },
-		browser: "Google Chrome Canary",
-		notify: false
-	});
+        browser: "Google Chrome Canary",
+        notify: false
+    });
 
-	gulp.watch(src + "scss/*.scss", ['sass']);
-	gulp.watch(src + "scss/*.scss").on("change", reload);
-	gulp.watch(src + "*.html").on("change", reload);
-	gulp.watch(src + "js/*.js").on("change", reload);
+    gulp.watch(src + "scss/**/*.scss", ['sass']);
+    gulp.watch(src + "scss/**/*.scss").on("change", reload);
+    gulp.watch(src + "*.html").on("change", reload);
+    gulp.watch(src + "js/**/*.js").on("change", reload);
 });
 
+// Compile the sass files into css files
+gulp.task("sass", function() {
+    gulp.src(src + "scss/styles.scss")
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest(src + 'css/')); // output to theme root
+});
 // Minify css
-gulp.task('minify-css', function(){
-    return gulp.src(src + 'css/*.css')
+gulp.task('minify-css', function() {
+    return gulp.src(src + 'css/styles.css')
         .pipe(cleanCSS())
         .pipe(gulp.dest(minsrc + 'css'));
 });
-
-// Compile the sass files
-gulp.task("sass", function() {
-    gulp.src(src + "scss/*.scss")
-        .pipe(sass())
-        .pipe(gulp.dest(src + 'css'));
-});
 // Autoprefix all the css
-gulp.task('prefix', () =>
+gulp.task('prefix', function() {
     gulp.src(minsrc + 'css/styles.css')
         .pipe(autoprefixer({
             browsers: ['last 10 versions'],
             cascade: false
         }))
-        .pipe(gulp.dest(minsrc + 'css/'))
-);
+        .pipe(gulp.dest(minsrc + 'css/'));
+});
 // Add bower componentes
 gulp.task('add-b', function() {
     return gulp.src('./bower.json')
@@ -66,66 +68,132 @@ gulp.task('add-b', function() {
 });
 // Concat all the js files
 gulp.task('concat-js', function() {
-  return gulp.src(src + 'js/*.js')
-    .pipe(concat('script.js'))
-    .pipe(gulp.dest(src + 'js/'));
+    return gulp.src(src + 'js/*.js')
+        .pipe(concat('script.js'))
+        .pipe(gulp.dest(src + 'js/'));
 });
 // Minify js
-gulp.task('minify-js', function (cb) {
-  pump([
+gulp.task('minify-js', function(cb) {
+    pump([
         gulp.src(src + 'js/script.js'),
         uglify(),
         gulp.dest(minsrc + 'js/')
     ],
-    cb
-  );
+        cb
+    );
 });
 // Minify html
 gulp.task('minify-html', function() {
-  return gulp.src(src + '*.html')
-    .pipe(htmlmin({collapseWhitespace: true}))
-    .pipe(gulp.dest(minsrc))
+    return gulp.src(src + '*.html')
+        .pipe(htmlmin({
+            collapseWhitespace: true
+        }))
+        .pipe(gulp.dest(minsrc));
 });
 // Compress images in src/img folder
-gulp.task('compress-img', () =>
-	gulp.src(src + 'img/*')
-		.pipe(imagemin())
-		.pipe(gulp.dest(minsrc + 'img'))
-);
-
-// Finish proyect
+gulp.task('compress-img', function() {
+    gulp.src(src + 'img/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest(minsrc + 'img'));
+});
 // Clean the finish proyect
-gulp.task('c-d', function () {
-  return del([
+gulp.task('c-d', function() {
+    return del([
     minsrc + '*.html',
     minsrc + '/js/*.js',
     minsrc + '/css/*.css',
     minsrc + '/img/*'
   ]);
 });
-// Compile, minified and compress all into the dist foler
-// NOT WORKING!! fails on sass task, not updated, ¡run twice!
-gulp.task('f', ['sass', 'concat-js', 'minify-js', 'minify-css', 'minify-html', 'compress-img'], function() {
-	console.log("Finished");
+
+// --------------
+// PRODUCTION
+// First prefix, then clean the dist folder, then do all the finish tasks
+// --------------
+gulp.task('c-d', ['f-prefix'], function() {
+    return del([
+    minsrc + '*.html',
+    minsrc + '/js/*.js',
+    minsrc + '/css/*.css',
+    minsrc + '/img/*'
+  ]);
+});
+// Autoprefix all the css
+gulp.task('f-prefix', function() {
+    gulp.src(src + 'css/*.css')
+        .pipe(autoprefixer({
+            browsers: ['last 10 versions'],
+            cascade: false
+        }))
+        .pipe(gulp.dest(src + 'css/'));
+});
+// Minify css
+gulp.task('f-minify-css', ['c-d'], function() {
+    return gulp.src(src + 'css/styles.css')
+        .pipe(cleanCSS())
+        .pipe(gulp.dest(minsrc + 'css'));
+});
+// Concat all the js files
+gulp.task('f-concat-js', ['c-d'], function() {
+    return gulp.src(src + 'js/*.js')
+        .pipe(concat('script.js'))
+        .pipe(gulp.dest(src + 'js/'));
+});
+// Minify js
+gulp.task('f-minify-js', ['c-d'], function(cb) {
+    pump([
+        gulp.src(src + 'js/script.js'),
+        uglify(),
+        gulp.dest(minsrc + 'js/')
+    ],
+        cb
+    );
+});
+// Minify html
+gulp.task('f-minify-html', ['c-d'], function() {
+    return gulp.src(src + '*.html')
+        .pipe(htmlmin({
+            collapseWhitespace: true
+        }))
+        .pipe(gulp.dest(minsrc));
+});
+// Compress images in src/img folder
+gulp.task('f-compress-img', ['c-d'], function() {
+    gulp.src(src + 'img/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest(minsrc + 'img'));
 });
 // Starts the server in the dist foler for testing
 gulp.task('check-dist', function() {
-	browserSync.init({
+    browserSync.init({
         server: {
             baseDir: [minsrc, '.tmp'],
-            routes:  {
+            routes: {
                 '/bower_components': 'bower_components'
             }
         },
-		browser: "Google Chrome Canary",
-		notify: false,
-        routes:{
-                    "../../bower_components" : "bower_components"
-                }
-	});
+        browser: "Google Chrome Canary",
+        notify: false,
+        routes: {
+            "../../bower_components": "bower_components"
+        }
+    });
 
-	gulp.watch(src + "scss/*.scss", ['sass']);
-	gulp.watch(src + "scss/*.scss").on("change", reload);
-	gulp.watch(src + "*.html").on("change", reload);
-	gulp.watch(src + "js/*.js").on("change", reload);
+    gulp.watch(src + "scss/*.scss", ['sass']);
+    gulp.watch(src + "scss/*.scss").on("change", reload);
+    gulp.watch(src + "*.html").on("change", reload);
+    gulp.watch(src + "js/*.js").on("change", reload);
+});
+
+// FINISH TASK
+gulp.task('f', ['f-concat-js', 'f-minify-js', 'f-minify-css', 'f-minify-html', 'f-compress-img', 'check-dist'], function() {
+    console.log("Created dist folder with dist files. -> PRODUCTION!");
+});
+
+// Experimento
+
+gulp.task('test', function() {
+    gulp.src(src + 'js/tests/*.js')
+        // gulp-jasmine works on filepaths so you can't have any plugins before it
+        .pipe(jasmine());
 });
